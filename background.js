07272a2b0d44b7996949e923d5875b505d12f207
José Victor Viriato <josevviriato@gmail.com>
2020-05-26 17:27:39 -0300

    add some tweaks

--- background.js
+++ background.js
@@ -2,8 +2,9 @@ import moment from 'moment'
 var working_hours = 8;
 var headers = document.getElementsByTagName('th');
 var table = document.getElementsByTagName('td');
+var tbody = document.getElementsByTagName('tbody')[0];
 var ht_morning_index = 0;
-var clock = `<i class="fa fa-clock-o fa-fw fa-lg" style="color: gray; opacity: 0.4" title="Original"></i>`
+var clock = `<i class="fa fa-clock-o fa-fw fa-lg" style="color: gray; opacity: 0.4" title="Hora Sugerida"></i>`
 var first_morning_hour_index = 1;
 var last_morning_hour_index = 2;
 var first_afternoon_hour_index = 5;
@@ -24,67 +25,70 @@ for (let i = 0; i < headers.length; i++) {
 var ht_morning = moment(table[ht_morning_index].innerHTML, 'HH:mm A');
 
 chrome.storage.sync.get(['working_hours'], function (result) {
-  working_hours = result.working_hours;
+  working_hours = result.working_hours || 8;
+  var halftime = working_hours / 2;
 
-
-  // quando há apenas a primeira batida
-  // TODO: inserir linhas seguintes
-  if (first_morning_hour.isValid() && !last_morning_hour.isValid()) {
+  var insertRow = function (label, first_hour, second_hour, ht) {
     var halftime = working_hours / 2;
-    var suggested_hour = first_morning_hour.add(halftime, 'hours').format('HH:mm[ h]');
+    var morning_tr = document.createElement('tr')
+    var morning = document.createElement('td')
+    var morning_first_hour = document.createElement('td')
+    var morning_second_hour = document.createElement('td')
+    var morning_ht = document.createElement('td')
+
+    morning.className = 'text-center'
+    morning_first_hour.className = 'text-left'
+    morning_second_hour.className = 'text-left'
+    morning_ht.className = 'text-center'
+
+    morning.appendChild(document.createTextNode(label))
+    morning_first_hour.innerHTML = clock + first_hour;
+    morning_second_hour.innerHTML = clock + second_hour
+    morning_ht.innerHTML = ht
+
+    morning_tr.appendChild(morning)
+    morning_tr.appendChild(morning_first_hour)
+    morning_tr.appendChild(morning_second_hour)
+    morning_tr.appendChild(morning_ht)
+    tbody.appendChild(morning_tr)
+  }
+
+  // Quando não há batidas, insere tudo
+  if (!first_morning_hour.isValid()) {
+    var first_hour_afternoon = moment().add(halftime + 1, 'hours')
+    insertRow('Manhã', moment().format('[ ]HH:mm'),
+      moment().add(halftime, 'hours').format('[ ]HH:mm'),
+      moment(halftime, 'H').format('HH:mm[ h]'));
+    insertRow('Tarde', first_hour_afternoon.format('HH:mm'),
+      first_hour_afternoon.add(halftime, 'hours').format('HH:mm'),
+      moment(working_hours, 'H').format('HH:mm[ h]')
+    );
+  }
+  // Quando há apenas a primeira batida
+  // TODO: inserir "Tarde"
+  else if (first_morning_hour.isValid() && !last_morning_hour.isValid()) {
+    var suggested_hour = first_morning_hour.add(halftime, 'hours').format('HH:mm');
     var suggested_hour_moment = first_morning_hour.add(halftime, 'hours')
     var worked_hours = moment(halftime, 'H').format('HH:mm[ h]')
-    table[last_morning_hour_index].innerHTML = clock + `<span style="color: gray; opacity: 0.4;">${suggested_hour}</span>`
+    table[last_morning_hour_index].innerHTML = clock + `<span style="color: gray; opacity: 0.4;"> ${suggested_hour}</span>`
     table[last_morning_hour_index].setAttribute('title', 'Hora Sugerida');
     var td = `<td class="text-left" style="white-space: nowrap" title="Hora Sugerida">${clock}<span style="color: gray; opacity: 0.4;">${worked_hours}</span></td>`
     table[last_morning_hour_index].insertAdjacentHTML('afterend', td);
-    // chrome.alarms.create('aviso_ponto', {
-    //   when: suggested_hour_moment.unix() * 1000
-    // })
-  } else if (first_afternoon_hour.isValid() && !last_afternoon_hour.isValid()) {
+    insertRow('Tarde', suggested_hour_moment.add(1, 'hours').format('HH:mm'),
+      suggested_hour_moment.add(halftime, 'hours').format('HH:mm'),
+      moment(working_hours, 'H').format('HH:mm[ h]')
+    );
+  }
+  // Quando falta a última batida
+  else if (first_afternoon_hour.isValid() && !last_afternoon_hour.isValid()) {
     var worked_hours = moment(working_hours, 'H').format('HH:mm[ h]')
     var duration = moment.duration({ hours: ht_morning.hours(), minutes: ht_morning.minutes() })
     var remaining_hours = moment(working_hours, 'H').subtract(duration);
     var minutes = remaining_hours.hours() * 60 + remaining_hours.minutes();
-    var suggested_hour = first_afternoon_hour.add(minutes, 'minutes').format('HH:mm[ h]');
-    table[last_afternoon_hour_index].innerHTML = clock + `<span style="color: gray; opacity: 0.4;">${suggested_hour}</span>`
+    var suggested_hour = first_afternoon_hour.add(minutes, 'minutes').format('HH:mm');
+    table[last_afternoon_hour_index].innerHTML = clock + `<span style="color: gray; opacity: 0.4;"> ${suggested_hour}</span>`
     table[last_afternoon_hour_index].setAttribute('title', 'Hora Sugerida');
     var td = `<td class="text-center" style="white-space: nowrap" title="Hora Sugerida"><span style="color: gray; opacity: 0.4;">${worked_hours}</span></td>`
     table[last_afternoon_hour_index].insertAdjacentHTML('afterend', td);
   }
 });
-
-// chrome.alarms.onAlarm.addListener(function (alarms) {
-//   alert('beep')
-//   alert(alarms)
-// })
-// var last_morning_hour_index = ht_morning_index - 2;
-// var last_morning_hour_index = ht_morning_index - 1;
-// var first_afternoon_hour_index = ht_morning_index + 2;
-// var last_afternoon_hour_index = ht_morning_index + 3;
-// var ht_afternoon_index = ht_morning_index + 4;
-// var ht_morning = moment(table[ht_morning_index].innerHTML, 'HH:mm A')
-
-// var first_afternoon_hour = moment(table[first_afternoon_hour_index].innerHTML, 'HH:mm A');
-// var last_afternoon_hour = moment(table[last_afternoon_hour_index].innerHTML, 'HH:mm A');
-
-
-// if (first_afternoon_hour.isValid() && !last_afternoon_hour.isValid()) {
-//   // calcula restante. adiciona sugestão na batida de saída de tarde 
-//   var duration = moment.duration({ hours: ht_morning.hours(), minutes: ht_morning.minutes() })
-//   var remaining_hours = moment(working_hours, 'H').subtract(duration).format('HH:mm[ h]');
-//   alert(remaining_hours.hours())
-// } else if (first_morning_hour.isValid() && !last_morning_hour.isValid()) {
-//   // adiciona sugestão de batida na volta do horário de almoço
-//   var suggested_hour = last_morning_hour.add('1', 'hour').format('HH:mm[ h]');
-//   table[first_afternoon_hour_index].setAttribute('title', 'Hora Sugerida')
-//   table[first_afternoon_hour_index].innerHTML = clock + `<span style="color: tomato; opacity: 0.4;">${suggested_hour}</span>`
-// } else {
-//   suggested_1st_hour = moment()
-//   suggested_2nd_hour = moment().add(working_hours / 2, 'hours')
-//   suggested_3rd_hour = 0 // diferenca hroas trabalhads  
-//   suggested_4nd_hour = suggested_3nd_hour.add(1, 'hours')
-//   suggested_5nd_hour = // restante de horas trabalhadas
-//   suggested_6nd_hour = moment().add(working_hours + 1, 'hours')  // ht2
-//   // adiciona todas batidas baseado no horário atual
-// }
